fd003ee4261d2ecdd093a9d5c914e33a917a74b5
gruman-itd <120284658+gruman-itd@users.noreply.github.com>
2022-12-12 11:16:01 -0800

    Second number bug

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,13 +1,29 @@
 let display = "";
 let currentValue = 0;
+let tempValue = 0;
 let operatorType = "";
+let clearMe = false;
+let editing = false;
 
 function pressButton(buttonValue) {
+    if (clearMe) {
+        clearDisplay();
+        clearMe = false;
+    }
+    if (operatorType != "" || editing === true) {
+        display = "";
+        editing = false;
+    }
+        
   display += buttonValue;
   document.getElementById("displayText").innerText = display;
+  currentValue = display;
 }
 
 function operators(type) {
+  tempValue = currentValue;
+  currentValue = 0;
+  editing = true;
   switch (type) {
     case "add":
       operatorType = "add";
@@ -25,11 +41,26 @@ function operators(type) {
 }
 
 function pressEnter() {
-  if ((operatorType = !"")) {
+    switch (operatorType) {
+      case "add":
+        tempValue = parseInt(tempValue) + parseInt(currentValue);
+        break;
+      case "subtract":
+        tempValue = parseInt(tempValue) - parseInt(currentValue);
+        break;
+      case "multiply":
+        tempValue = parseInt(tempValue) * parseInt(currentValue);
+        break;
+      case "divide":
+        tempValue = parseInt(tempValue) / parseInt(currentValue);
+        break;
+    }
     operatorType = "";
-    document.getElementById("displayText").innerText = currentValue;
+    document.getElementById("displayText").innerText = tempValue;
     currentValue = 0;
-  }
+    tempValue = 0;
+    clearMe = true;
+    editing = false;
 }
 
 function clearDisplay() {

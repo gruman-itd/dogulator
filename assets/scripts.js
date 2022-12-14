b30075303bc0f57ca53d27ab3d62db18195073ba
gruman-itd <120284658+gruman-itd@users.noreply.github.com>
2022-12-14 11:21:37 -0800

    Dogs and operators
    Operators show results
    Dogs on h1 click

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,66 +1,17 @@
-let display = ""; // calculator display
-let currentValue = 0; // first value
-let tempValue = 0; // second value
-let operatorType = ""; // operator
-let clearMe = false; // set to clear display on button press
-let editing = false; // entering the second number
+function getNumber(btn) {
+  let solution = document.getElementById("displayText").innerText;
 
-function pressButton(buttonValue) {
-  // press button
-  if (clearMe) {
-    // clear everything
-    clearDisplay();
-    clearMe = false;
+  if (btn == "enter") {
+    // results
+    document.getElementById("displayText").innerText = eval(solution);
+  } else {
+    // add number to display
+    let number = document.getElementById(btn).value;
+	document.getElementById("hiddenOp").value = number;
+    document.getElementById("displayText").innerText = solution + number;
   }
-  if (editing) {
-    // clear display for second number
-    display = "";
-    editing = false;
-  }
-  // concat the numbers
-  display += buttonValue;
-  // set display
-  document.getElementById("displayText").innerText = display;
-  // store current value
-  currentValue = display;
-}
-
-function operators(type) {
-  // pressed an operatpr
-  tempValue = currentValue; // store first number
-  currentValue = 0; // reset
-  editing = true; // set to enter second value
-  operatorType = type; // set operatorType
-}
-
-function pressEnter() {
-  // equals button
-  switch (
-    operatorType // do caulations based on operatorType
-  ) {
-    case "add":
-      tempValue = parseInt(tempValue) + parseInt(currentValue);
-      break;
-    case "subtract":
-      tempValue = parseInt(tempValue) - parseInt(currentValue);
-      break;
-    case "multiply":
-      tempValue = parseInt(tempValue) * parseInt(currentValue);
-      break;
-    case "divide":
-      tempValue = parseInt(tempValue) / parseInt(currentValue);
-      break;
-  }
-  operatorType = ""; // clear operatorType
-  document.getElementById("displayText").innerText = tempValue; // set display
-  currentValue = 0; // reset
-  tempValue = 0; // reset
-  clearMe = true; // clear value on press
-  editing = false; // clear editing
 }
 
 function clearDisplay() {
-  // clear the display
-  display = "";
-  document.getElementById("displayText").innerText = "0";
+  document.getElementById("displayText").innerText = "";
 }
